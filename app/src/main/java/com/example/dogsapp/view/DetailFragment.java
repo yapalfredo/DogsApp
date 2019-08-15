c16f0508fca7219d9201a2c08b095e8aa0884f2f
al <alfredo.yap@stu.bmcc.cuny.edu>
2019-08-14 20:57:52 -0400

    Finished 5.) Layouts

---
+++ app/src/main/java/com/example/dogsapp/view/DetailFragment.java
@@ -0,0 +1,52 @@
+package com.example.dogsapp.view;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.dogsapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class DetailFragment extends Fragment {
+
+    private int dogUuid;
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (getArguments() != null){
+            dogUuid = DetailFragmentArgs.fromBundle(getArguments()).getDogUuid();
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/dogsapp/view/ListFragment.java
@@ -0,0 +1,39 @@
+package com.example.dogsapp.view;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dogsapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class ListFragment extends Fragment {
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+}
